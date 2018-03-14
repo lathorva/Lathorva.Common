32fc76e6d86aa9f256327ec7eabbb5b18a28ddca
lathorva <lathorva@gmail.com>
2018-03-14 19:55:59 +0100

    Added datepart

---
+++ Lathorva.Common/Enums/DatePart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Lathorva.Common.Enums
+{
+    public enum DatePart
+    {
+        Unknown = 0,
+        [Description("År")]
+        Year = 1,
+        [Description("Måned")]
+        Month = 2,
+        [Description("Uke")]
+        Week = 3,
+        [Description("Dag")]
+        Day = 4,
+
+        [Description("Ukedag")]
+        DayOfWeek = 5,
+
+        [Description("Time")]
+        Hour = 6,
+    }
+}
